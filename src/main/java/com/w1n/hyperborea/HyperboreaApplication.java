540512e1a8ee1615f7c47eab5af004d7ace54091
w1nsun <ilya.nevmyvaka@gmail.com>
2023-10-26 23:23:29 +0200

    init

---
+++ src/main/java/com/w1n/hyperborea/HyperboreaApplication.java
@@ -0,0 +1,11 @@
+package com.w1n.hyperborea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperboreaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HyperboreaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/w1n/hyperborea/HyperboreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w1n.hyperborea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperboreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
